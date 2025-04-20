26691399a189c9c96be3a22db8b41949356e39a5
Carmen Arreguin <car252004@gmail.com>
2025-04-19 21:32:55 -0600

    Creación del repositorio

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:tap2025/screens/contador_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ContadorScreen(),
+    );
+  }
+}
